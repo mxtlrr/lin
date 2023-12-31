827bd394320d79f9c24b41636bf8fd676575a78c
mxtlrr <ruson0914@gmail.com>
2023-12-31 13:18:01 -0600

    bas e  ncurses

--- src/lin.py
+++ src/lin.py
@@ -1,4 +1,6 @@
 import sqlite3
+from curses import *
+
 con = sqlite3.connect("lin.db")
 cur = con.cursor()
 
@@ -15,8 +17,7 @@ class LinDB:
             cur.execute("CREATE TABLE Liquids(Name VARCHAR, Formula VARCHAR, Volume FLOAT);")
             cur.execute("CREATE TABLE Solids(Name VARCHAR, Formula VARCHAR, Volume FLOAT);")
             cur.execute("CREATE TABLE GlassHardware(Name VARCHAR, Count INT);")
-        except sqlite3.OperationalError as e:
-            print("[NONFATAL] sqlite3:", e)
+        except sqlite3.OperationalError as e: print("[NONFATAL] sqlite3:", e)
 
     def WriteTableReagent(table: int, name: str, formula: str, quant: float) -> None:
         cur.execute(f"SELECT * FROM {table} WHERE Name='{name}'")
@@ -25,7 +26,6 @@ class LinDB:
         if result: # exists, update.
             cur.execute(f"UPDATE {table} SET Name = '{name}', Formula = '{formula}', Volume = {quant}")
             con.commit()
-            return
         else:
             cur.execute(f"INSERT INTO {table} VALUES ('{name}', '{formula}', {quant});")
             con.commit() # make sure our changes our committed into
@@ -38,7 +38,6 @@ class LinDB:
         if result: # exists, update.
             cur.execute(f"UPDATE GlassHardware SET Name = '{name}', Formula = '{formula}', Volume = {quant}")
             con.commit()
-            return
         else:
             cur.execute(f"INSERT INTO GlassHardware VALUES ('{name}', {count}")
             con.commit()
@@ -53,11 +52,36 @@ class LinDB:
         if table == 1: return cur.execute(f"SELECT * FROM Solids").fetchall()
         if table == 2: return cur.execute(f"SELECT * FROM GlassHardware").fetchall()
 
-def main():
-    LinDB.WriteTableReagent('Liquids', "Acetonitrile", "CH3CN", 450.47)
-    for rows in LinDB.GetAll(LIQUIDS):
-        print(rows)
+def clear(stdscr) -> None:
+    stdscr.erase()
+    stdscr.refresh()
 
+def main():
+    """ There are four tabs:
+        1: Liquids Table
+        2: Solids Table
+        3: Glassware Table
+        4: Mini CLI """
+    tab = 1
+    tab_arr = ["", "Liquids Table", "Solids Table", "Glassware Table", "Mini CLI"]
+
+    stdscr = initscr()
+
+    while True:
+        stdscr.addstr(f"<{tab}/4> {tab_arr[tab]}")
+        c = stdscr.getch()
+        if c == 101: break
+        if c == 112: # previous
+            if tab != 1: tab -= 1
+            clear(stdscr)
+
+        if c == 110: # next
+            if tab != 4: tab += 1
+            clear(stdscr)
+
+        else: clear(stdscr)
+
+    endwin()
 
 if __name__ == "__main__":
     LinDB.MakeTables()
